c2468661b52dc3a99076ea68365013e76bceb28a
Frieder Grießhammer <frieder.griesshammer@der-beweis.de>
2012-09-04 13:22:09 +0200

    Umlaute sind Luxus

--- programmieren lernen/cpp/quelldateien/03_zahlenraten_kommentiert.cpp
+++ programmieren lernen/cpp/quelldateien/03_zahlenraten_kommentiert.cpp
@@ -1,7 +1,7 @@
 /**
-Das ist das Spiel "Zahlenraten" (oder "guess the number" in englisch)
+Das ist das Spiel "Zahlenraten" (oder "guess the number" in Englisch)
 
-Das Programm denkt sich eine Zahl aus, die der Spieler moeglichst schnell erraten muss
+Das Programm denkt sich eine Zahl aus, die der Spieler möglichst schnell erraten muss
 **/
 
 #include <iostream>
@@ -14,44 +14,45 @@ int main(void){
 
 	int gesucht, eingabe, runde = 0;
 
-	// Wird fuer den Zufall benoetigt
+	// Der Zufallszahlengenerator wird mit der Systemzeit initialisiert
+	// Setzt also einen Startwert
 	srand(time(NULL));
 
-	// Zahl zwichen 1 und 20 "ausdenken"
+	// Zahl zwischen 1 und 20 "ausdenken"
 	gesucht = rand() % 20 + 1;
 
 	cout << endl << "Ich habe mir eine Zahl zwischen 1 und 20 ausgedacht.";
 
-	// Den Spieler hoechstens 6 Runden raten lassen
 	do{
 		// Eine Runde mehr
 		runde++;
 
-		// Spieler eine Zahl raten lassen ...
+		// Den Spieler eine Zahl raten lassen ...
 		cout << endl << "Du darfst jetzt raten: > ";
-		// .. und in "eingabe" speichern
+		// ... und die Zahl in "eingabe" speichern
 		cin >> eingabe;
 
-		// Wenn die Zahl kleiner als die gedachte Zahl ist
+		// Wenn die Zahl kleiner als die gedachte Zahl ist:
 		if(eingabe < gesucht){
 			cout << endl << "Du hast zu niedrig geraten.";
-		// Wenn die Zahl groesser als die gedachte Zahl ist
+		// Wenn die Zahl groesser als die gedachte Zahl ist:
 		}else if(eingabe > gesucht){
 			cout << endl << "Du hast zu hoch geraten.";
-		// Wenn die Zahl gleich der gedachten Zahl ist
+		// Wenn die Zahl gleich der gedachten Zahl ist:
 		}else if(eingabe == gesucht){
 			cout << endl << "Gut gemacht, du hast die Zahl in " << runde << " Runden erraten!";
-			//Schleife unterbrechen
+			// Der Spieler hat gewonnen! Deshalb wird die Schleife abgebrochen
 			break;
 		}
-		
+	// Den Spieler höchstens 6 Runden raten lassen
 	}while(runde < 6);
 
-	// Wenn die Zahl am Schluss immernoch falsch ist
+	// Wenn die Zahl am Schluss immernoch falsch ist:
 	if(eingabe != gesucht){
 		cout << endl << "Falsch. Ich habe an die " << gesucht << " gedacht...";
 	}
 
-        return 0;
+	// Programm ordnungsgemäß beenden
+	return 0;
 }
 
